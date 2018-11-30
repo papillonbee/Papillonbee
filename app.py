a4febeacdbfca3cd8ad3fa64d2abb22646384d21
Papan Yongmalwong <37870054+papillonbee@users.noreply.github.com>
2018-11-30 22:31:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def bot():
             line_bot_api.leave_group(Id)
     else:
         reply(replyToken, replyStack[:5], messageType)
-        push(userId, msg_in_json)
+        reply(replyToken, [msg_in_json])
     
     
     
