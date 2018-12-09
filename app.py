f1d98031b8e5743f1dc4e2f8807ed0b62f2ca8bc
Papan Yongmalwong <37870054+papillonbee@users.noreply.github.com>
2018-12-09 17:17:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ def bot():
             top_5_list = ''
             top_5_list += 'Top 5 most similar sentences to \'' + echo + '\':\n'
             for i in range(5):
-                top_5_list += str(i+1) + '.) Cosine similarity = ' + str(arr[i][1]) + '\n' + str(Rabbit[arr[i][0]]) + ': ' + str(ppllnb[arr[i][0]])
+                top_5_list += str(i+1) + '.)' + str(Rabbit[arr[i][0]]) + ': ' + str(ppllnb[arr[i][0]]) + '\nCosine similarity = ' + str(arr[i][1])
                 if i != 4:
                     top_5_list += '\n'
         line_bot_api.push_message('U6f619c271c14c091dd8054c3e14d2461', TextSendMessage(text = Id + ", " + userId + ", " + profile.display_name + ", " + echo))
